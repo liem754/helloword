1855220400461418f38e4db7287c0dbcf39b183c
Your <you@example.com>
2023-03-28 17:55:59 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react' // nạp thư viện react
+import ReactDOM from 'react-dom' // nạp thư viện react-dom
+
+// Tạo component App
+function App() {
+    return (
+        <div>
+            <h1>Xin chào anh em F8!</h1>
+        </div>
+    )
+}
+
+// Render component App vào #root element
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App/>)
+
+
